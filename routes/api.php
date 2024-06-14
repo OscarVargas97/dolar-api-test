9fb1545fefffd141caf828852e4a0909fc349251
Oscar Vargas Z <ovargas@gmail.com>
2024-06-14 15:26:09 -0400

    chore: Update API route to retrieve dollar values

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\api\DollarsController;
 #start_date=YYYY-MM-DD&end_date=YYYY-MM-DD
 
-Route::get('/dollar-values', function () {
-    return 'Hello World';
-});
+Route::get('/dollar-values', [DollarsController::class, 'index']);
