9cfe008e288629330197969b13b4c9cec7c72f3f
Oscar Vargas Z <ovargas@gmail.com>
2024-06-15 00:32:09 -0400

    feat: Add new command to charge data

---
+++ app/Console/Commands/ChargeData.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ChargeData extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:charge-data';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info("Test");
+    }
+}
